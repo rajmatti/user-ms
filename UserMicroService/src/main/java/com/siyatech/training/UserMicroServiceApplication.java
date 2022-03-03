a39c1263dccf7011fe3d1150805a9d2849072414
Raj gs <rajmatti@gmail.com>
2022-03-03 16:04:53 +0530

    us commit

---
+++ UserMicroService/src/main/java/com/siyatech/training/UserMicroServiceApplication.java
@@ -0,0 +1,15 @@
+package com.siyatech.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroServiceApplication.class, args);
+	}
+
+}
